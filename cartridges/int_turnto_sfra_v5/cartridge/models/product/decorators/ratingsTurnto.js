cc4133b07b88b051e118c65fb8e815032950f2c8
rholgate_turnto <62255733+rholgate-turnto@users.noreply.github.com>
2022-01-18 09:24:20 -0800

    Update ratingsTurnto.js
    Remove test log writes

--- cartridges/int_turnto_sfra_v5/cartridge/models/product/decorators/ratingsTurnto.js
+++ cartridges/int_turnto_sfra_v5/cartridge/models/product/decorators/ratingsTurnto.js
@@ -3,10 +3,8 @@
 var Logger = require('dw/system/Logger');
 
 module.exports = function (object, apiProduct) {
-    Logger.info('INFO - TTTEST - in ratingsTurnto');
     Object.defineProperty(object, 'starRating', {
         enumerable: true,
         value: (Object.prototype.hasOwnProperty.call(apiProduct.custom, 'turntoAverageRating') && apiProduct.custom.turntoAverageRating) ? apiProduct.custom.turntoAverageRating : 0
     });
-    Logger.info('INFO - TTTEST - in ratingsTurnto2');
 };
