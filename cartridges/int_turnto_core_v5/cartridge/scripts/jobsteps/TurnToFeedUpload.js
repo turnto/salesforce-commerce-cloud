a8b6b57548a9cefd03274ee700b3c57c578f08e2
Preston Rollins <preston.rollins@pixleeturnto.com>
2024-06-10 17:59:25 -0400

    TSD-212: Feed Upload Job Error Handling; continue loops on error; improve error messages

--- cartridges/int_turnto_core_v5/cartridge/scripts/jobsteps/TurnToFeedUpload.js
+++ cartridges/int_turnto_core_v5/cartridge/scripts/jobsteps/TurnToFeedUpload.js
@@ -30,6 +30,11 @@ var ServiceFactory = require('*/cartridge/scripts/util/ServiceFactory');
  * @returns {dw.system.Status} The exit status for the job step
  */
 var run = function run() {
+    var statusData = {
+        status: Status.OK,
+        code: 'OK',
+        message: 'Upload successful.'
+    }
     try {
         var args = arguments[0];
 
@@ -47,7 +52,12 @@ var run = function run() {
 
         // Test mandatory parameters
         if (empty(serviceID) || empty(postFileLocation) || empty(filePattern)) {
-            return new Status(Status.ERROR, 'ERROR', 'One or more mandatory parameters are missing. Service ID = (' + serviceID + ') Post File Location = (' + postFileLocation + ') FilePattern = (' + filePattern + ')');
+            return new Status(
+                Status.ERROR,
+                'ERROR',
+                'One or more mandatory parameters are missing. Service ID = (' + serviceID + ') Post File Location = (' +
+                postFileLocation + ') FilePattern = (' + filePattern + ')'
+            );
         }
 
         // Get the file path where the output will be stored
@@ -62,85 +72,109 @@ var run = function run() {
         // Retrieve HashMap of Keys which contain auth and site keys
         var siteAndAuthKeys = TurnToHelper.getHashMapOfKeys();
         if (!siteAndAuthKeys) {
-            return new Status(Status.ERROR, 'ERROR', 'Did not find SiteAuthKeyJSON value for site');
+            return new Status(Status.ERROR, 'ERROR', 'Custom preference turntoSiteAuthKeyJSON value for site not set');
         }
 
         // Loop through all allowed locales per site
         var allowedLocales = TurnToHelper.getAllowedLocales();
         for (var i = 0; i < allowedLocales.length; i++) {
             var locale = allowedLocales[i];
-            // List all files in the specific locale folder
-            var exportFiles = new File(turntoDir.getFullPath() + File.SEPARATOR + locale);
-            exportFiles = exportFiles.listFiles();
+            try {
+                // List all files in the specific locale folder
+                var exportFiles = new File(turntoDir.getFullPath() + File.SEPARATOR + locale);
+                exportFiles = exportFiles.listFiles();
 
-            // If no files are found, go to the next locale
-            if (exportFiles) {
-                // filter out files which match the file pattern
-                var filteredExportFiles = exportFiles.clone();
-                for (var k = 0; k < exportFiles.size(); k++) {
-                    var file = exportFiles[k];
-                    if (file.getName().match(filePattern) == null) {
-                        filteredExportFiles.remove(file);
-                    } else {
-                        // Localized siteKey and authKey, need to download per locale, added to the content body of the service request
-                        var ttSiteSet = siteAndAuthKeys.entrySet();
-                        var siteKey = null;
-                        var authKey = null;
-                        var domain = null;
-                        for (var ls = 0; ls < ttSiteSet.size(); ls++) {
-                            if (ttSiteSet[ls].value.locales.indexOf(locale) > -1) {
-                                siteKey = JSON.parse(ttSiteSet[ls].key);
-                                authKey = ttSiteSet[ls].value.authKey;
-                                domain = 'www.' + ttSiteSet[ls].value.domain;
-                                break;
-                            }
-                        }
-
-                        // If turntoAuthKey and turntoSiteKey values are not defined for a particular locale the job should skip the locale.
-                        if (empty(siteKey) || empty(authKey) || empty(domain)) {
-                            Logger.error('FAILED Site and/or Auth key or domain is missing: Site Key -> ' + siteKey + ' Auth Key -> ' + authKey + ' Export File Name -> ' + file.name + ' domain -> ' + domain);
+                // If no files are found, go to the next locale
+                if (exportFiles) {
+                    // filter out files which match the file pattern
+                    var filteredExportFiles = exportFiles.clone();
+                    for (var k = 0; k < exportFiles.size(); k++) {
+                        var file = exportFiles[k];
+                        if (file.getName().match(filePattern) == null) {
+                            filteredExportFiles.remove(file);
                         } else {
-                            // feedUploadService
-                            var requestDataContainer = ServiceFactory.buildFeedUploadRequestContainer(postFileLocation, file, siteKey, authKey, domain, feedStyle);
-                            if (logging) {
-                                Logger.info('Post file location: ' + postFileLocation);
-                                Logger.info('Export file: ' + file);
-                            }
-
-                            // call service, returns success or error
-                            var feedUploadResult = FeedUploadService.call(requestDataContainer);
-
-                            if (feedUploadResult.error) {
-                                Logger.error('Feed upload service error: ' + feedUploadResult.error + ' ' + feedUploadResult.errorMessage);
-                            }
-
-                            if (feedUploadResult.isOk()) {
-                                // Archive file
-                                var archiveFile = new File(turntoDir.fullPath + File.SEPARATOR + 'Archive');
-
-                                if (!archiveFile.exists()) {
-                                    archiveFile.mkdirs();
+                            // Localized siteKey and authKey, need to download per locale, added to the content body of the service request
+                            var ttSiteSet = siteAndAuthKeys.entrySet();
+                            var siteKey = null;
+                            var authKey = null;
+                            var domain = null;
+                            for (var ls = 0; ls < ttSiteSet.size(); ls++) {
+                                if (ttSiteSet[ls].value.locales.indexOf(locale) > -1) {
+                                    siteKey = JSON.parse(ttSiteSet[ls].key);
+                                    authKey = ttSiteSet[ls].value.authKey;
+                                    domain = 'www.' + ttSiteSet[ls].value.domain;
+                                    break;
                                 }
-                                var theArchiveFile = new File(archiveFile.fullPath + File.SEPARATOR + file.getName());
-                                file.renameTo(theArchiveFile);
+                            }
+
+                            // If turntoAuthKey and turntoSiteKey values are not defined for a particular locale the job should skip the locale.
+                            if (empty(siteKey) || empty(authKey) || empty(domain)) {
+                                Logger.error('FAILED Missing Site key, Auth key, or domain for locale "' + locale + '".' +
+                                    ' Check turntoSiteAuthKeyJSON site preference. Site Key: ' + siteKey +
+                                    ', Auth Key: ' + authKey + ' Domain: ' + domain + ', Export File Name: ' + file.name);
+                                statusData.status = Status.ERROR;
+                                statusData.code = 'ERROR'
+                                statusData.message += ' - Locale: ' + locale + '. Error: Missing Site key, Auth key, or ' +
+                                    'domain for locale in turntoSiteAuthKeyJSON site preference. Site Key: ' + siteKey +
+                                    ', Auth Key: ' + authKey + ' Domain: ' + domain + ', Export File Name: ' + file.name;
                             } else {
-                                return new Status(Status.ERROR, 'ERROR', 'FAILED uploading file with XML file name : ' + file.fullPath + '\nerror:' + feedUploadResult.errorMessage);
+                                // feedUploadService
+                                var requestDataContainer =
+                                    ServiceFactory.buildFeedUploadRequestContainer(postFileLocation, file, siteKey, authKey, domain, feedStyle);
+                                if (logging) {
+                                    Logger.info('Post file location: ' + postFileLocation);
+                                    Logger.info('Export file: ' + file);
+                                }
+
+                                // call service, returns ok or error
+                                var feedUploadResult = FeedUploadService.call(requestDataContainer);
+                                if (feedUploadResult.isOk()) {
+                                    // Archive file
+                                    var archiveFile = new File(turntoDir.fullPath + File.SEPARATOR + 'Archive');
+
+                                    if (!archiveFile.exists()) {
+                                        archiveFile.mkdirs();
+                                    }
+                                    var theArchiveFile = new File(archiveFile.fullPath + File.SEPARATOR + file.getName());
+                                    file.renameTo(theArchiveFile);
+                                } else {
+                                    statusData.status = Status.ERROR;
+                                    statusData.code = 'ERROR';
+                                    statusData.message += ' - Locale: ' + locale + '. Error: Failed to upload XML file ';
+                                    if (feedUploadResult.errorMessage) {
+                                        statusData.message += file.fullPath + ' - Error message: ' + feedUploadResult.errorMessage;
+                                        Logger.error('Feed upload service error for locale "' + locale + '" Error: ' +
+                                            feedUploadResult.error + ' ' + feedUploadResult.errorMessage);
+                                    }
+                                    if (feedUploadResult.unavailableReason) {
+                                        statusData.message += ' - Unavailable Reason: ' + feedUploadResult.unavailableReason;
+                                        Logger.error('Feed upload service unavailable for locale "' + locale + '" Reason: ' +
+                                            feedUploadResult.unavailableReason);
+                                    }
+                                }
                             }
                         }
                     }
-                }
 
-                if (filteredExportFiles.size() === 0) {
-                    if (noFilesFoundStatus === 'ERROR') {
-                        return new Status(Status.ERROR, 'ERROR', 'FAILED No files existed with file pattern: ' + filePattern);
+                    if (filteredExportFiles.size() === 0 && noFilesFoundStatus === 'ERROR') {
+                        statusData.status = Status.ERROR;
+                        statusData.code = 'ERROR'
+                        statusData.message += ' - Locale: ' + locale + '. Error: No files existed with file pattern: ' + filePattern;
                     }
                 }
+            } catch (exception) {
+                statusData.status = Status.ERROR;
+                statusData.code = 'ERROR'
+                statusData.message += ' - Locale: ' + locale + '. Error: ' + exception.message;
             }
         }
     } catch (exception) {
-        return new Status(Status.ERROR, 'ERROR', 'FAILED Upload failed with catch block. Error message: ' + exception.message);
+        statusData.status = Status.ERROR;
+        statusData.code = 'ERROR'
+        statusData.message += ' - Error: ' + exception.message;
     }
-    return new Status(Status.OK, 'OK', 'Upload successful.');
+
+    return new Status(statusData.status, statusData.code, statusData.message);
 };
 
 exports.Run = run;
