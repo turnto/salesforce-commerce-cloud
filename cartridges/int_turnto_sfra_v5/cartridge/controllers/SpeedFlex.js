bc84fb237e119c874a8824cfb29ddd7652caa811
Preston Rollins <preston.rollins@pixleeturnto.com>
2025-09-22 14:10:06 -0400

    release/1.4.6 (#83)
    * TSD-379: Update teaser in SFRA; update turnto template div id; remove unused product template (#84)
    
    * IMP-4182: SSO update;  (#85)
      * Added function to register on page load for return from login; Added redirect controller for single path for all redirects because login redirects are limited by oAuthRenentryRedirectEndpoints so controller must be added to this array; controller validates redirects for security
    
      * replace default redirect with frontend console error to help with imp debugging; Add redirect endpoint to work out of the box; remove path check which may not work for all setups and domain is already checked
    
      * refactor sitegen SpeedFlex controller to add redirect endpoint to store and use referrer url to redirect back to pdp; moved validation to Util; improve logging
    
      * only get redirect url when needed

--- cartridges/int_turnto_sfra_v5/cartridge/controllers/SpeedFlex.js
+++ cartridges/int_turnto_sfra_v5/cartridge/controllers/SpeedFlex.js
@@ -1,29 +1,72 @@
 'use strict';
 
 var server = require('server');
+var URLUtils = require('dw/web/URLUtils');
+var Logger = require('dw/system/Logger');
+var CustomerMgr = require('dw/customer/CustomerMgr');
+
+// Load additional dependencies
+var authHelper = require('*/cartridge/scripts/util/AuthHelper');
+var endpoints = require('*/cartridge/config/oAuthRenentryRedirectEndpoints');
+var TurnToHelper = require('*/cartridge/scripts/util/TurnToHelperUtil');
+
 
 server.post('UserData', server.middleware.https, function (req, res, next) {
-    var URLUtils = require('dw/web/URLUtils');
-    var authHelper = require('*/cartridge/scripts/util/AuthHelper');
-
-    var target = request.getHttpReferer();
-    var redirectUrl = URLUtils.url('Login-Show', 'rurl', target).toString();
-
     var customer = session.getCustomer();
     var userAuthToken = session.getSessionID();
     var isUserLoggedIn = customer.authenticated && customer.registered;
     var userDataToken = isUserLoggedIn ? authHelper.getUserDataToken(customer, userAuthToken) : null;
 
+    if (isUserLoggedIn) {
+        res.json({
+            isUserLoggedIn: true,
+            userDataToken: userDataToken
+        });
+        return next();
+    }
+
+    var target = request.getHttpReferer();
+    var validatedUrl = TurnToHelper.validateRedirectUrl(target);
+    if (!validatedUrl) {
+        Logger.error('SpeedFlex-UserData: Invalid or missing redirect URL: {0}', target);
+        res.json({
+            error: true,
+            errorMessage: 'Invalid or missing redirect URL',
+            isUserLoggedIn: false
+        });
+        return next();
+    }
+
+    var rurlEndpoint = null;
+
+    Object.keys(endpoints).forEach(function(key) {
+        if (endpoints[key] === 'SpeedFlex-LoginRedirect') {
+            rurlEndpoint = parseInt(key, 10);
+        }
+    });
+
+    if (!rurlEndpoint) {
+        Logger.error('SpeedFlex-UserData: SpeedFlex-LoginRedirect endpoint not found in oAuthRenentryRedirectEndpoints');
+        res.json({
+            error: true,
+            errorMessage: 'Redirect endpoint not configured',
+            isUserLoggedIn: false
+        });
+        return next();
+    }
+
+    // Store the validated URL and return login redirect
+    req.session.privacyCache.set('speedFlexRedirectUrl', validatedUrl);
+    var redirectUrl = URLUtils.url('Login-Show', 'rurl', rurlEndpoint).toString();
+
     res.json({
-        isUserLoggedIn: isUserLoggedIn,
-        redirectUrl: redirectUrl,
-        userDataToken: userDataToken
+        isUserLoggedIn: false,
+        redirectUrl: redirectUrl
     });
     return next();
 });
 
 server.post('LoggedInData', server.middleware.https, function (req, res, next) {
-    var authHelper = require('*/cartridge/scripts/util/AuthHelper');
 
     var customer = session.getCustomer();
     var userAuthToken = session.getSessionID();
@@ -36,11 +79,22 @@ server.post('LoggedInData', server.middleware.https, function (req, res, next) {
 });
 
 server.post('LoggedOut', server.middleware.https, function (req, res, next) {
-    var CustomerMgr = require('dw/customer/CustomerMgr');
     CustomerMgr.logoutCustomer(false);
     res.json({
         isUserLoggedIn: false
     });
+    return next();
+});
+
+server.get('LoginRedirect', function (req, res, next) {
+    var redirectUrl = req.session.privacyCache.get('speedFlexRedirectUrl');
+    if (redirectUrl) {
+        req.session.privacyCache.set('speedFlexRedirectUrl', null);
+        res.redirect(redirectUrl);
+    } else {
+        res.redirect(URLUtils.url('Account-Show'));
+    }
+
     return next();
 });
 
